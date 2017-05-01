0279e9a6fcbf2ce9b17f389b2cd0d92e420c599f
unknown <you@example.com>
2017-05-01 10:46:39 +0300

    header top

--- app/include/header.php
+++ app/include/header.php
@@ -2,19 +2,28 @@
 	<div class="az-header-top">
 		<div class="container">
 			<div class="row">
-				<div class="hidden-xs col-sm-1">
+				<div class="hidden-xs col-sm-1 col-md-2 col-lg-2">
 					<div class="az-lang">
 						<a href="#">RU</a>
 						<span>/</span>
 						<a href="#">ENG</a>
 					</div>
 				</div>
-				<div class="col-xs-12 col-sm-11">
+				<div class="col-xs-12 col-sm-11 col-md-10 col-lg-8">
 					<nav class="az-main-menu">
 						<a href="#" class="az-menu-icon">Меню</a>
 						<ul class="az-menu-list">
 							<li><a href="#" class="active">Главная</a></li>
-							<li><a href="#">Услуги</a></li>
+							<li>
+								<a href="#" class="submenu">Услуги</a>
+								<ul>
+									<li><a href="#">Услуга1</a></li>
+									<li><a href="#">Услуга2</a></li>
+									<li><a href="#">Услуга3</a></li>
+									<li><a href="#">Услуга4</a></li>
+									<li><a href="#">Услуга5</a></li>
+								</ul>
+							</li>
 							<li><a href="#">О нас</a></li>
 							<li><a href="#">Новости и статьи</a></li>
 							<li><a href="#">Отзывы и рекомендации</a></li>
@@ -22,7 +31,13 @@
 						</ul>
 					</nav>
 				</div>
-				<div class="hidden-xs hidden-sm"></div>
+				<div class="hidden-xs hidden-sm hidden-md col-lg-2">
+					<div class="az-log-reg">
+						<a href="#">вход</a>
+						<span>/</span>
+						<a href="#">регистрация</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- app/js/common2.js
+++ app/js/common2.js
@@ -2,11 +2,22 @@ $(function() {
 
 	$(window).click(function(){
 		$(".az-menu-list").removeClass("on");
+		$(".az-menu-list ul").removeClass("on");
 	});
 
 	$(".az-menu-icon").click(function(event){
 		event.stopPropagation();
 		$(".az-menu-list").toggleClass("on");
+		if(!$(".az-menu-list").hasClass("on")){
+			$(".az-menu-list ul").removeClass("on");
+		}
+	});
+
+	$(".az-menu-list a").click(function(event){
+		event.stopPropagation();
+		if($(this).next("ul").length>0){
+			$(this).next("ul").toggleClass("on");
+		}
 	});
 	
 });
