f52f535a732f2b2e0b92c465bfdb0a0abb089105
unknown <you@example.com>
2017-05-01 12:16:03 +0300

    header bottom

--- app/include/header.php
+++ app/include/header.php
@@ -41,4 +41,36 @@
 			</div>
 		</div>
 	</div>
+	<div class="az-header-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
+					<div class="az-logo">
+						<img src="img/az-logo.png" alt="InterJustLtd">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-5 col-md-5 col-lg-3 pull-right">
+					<div class="az-contacts">
+						<div><a href="tel:88129991990" class="az-phone">8 (812) <strong>999-19-90</strong></a></div>
+						<div><span class="az-worktime">Пн-Пт с 09:00 до 19:00 Мск</span></div>
+						<div class="hidden-xs hidden-sm hidden-md"><a href="#" class="az-call-back">заказать обратный звонок</a></div>
+					</div>
+				</div>
+				<div class="col-sm-3 hidden-xs az-center col-md-3 hidden-lg">
+					<div class="az-log-reg">
+						<a href="#">вход</a>
+						<span>/</span>
+						<a href="#">регистрация</a>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+					<div class="az-header-links clearfix">
+						<div class="az-header-link-block1 pull-right hidden-lg"><a href="#" class="az-header-link">заказать обратный звонок</a></div>
+						<div class="az-header-link-block2 pull-left"><a href="#" class="az-header-link2">Записаться на первичную консультацию</a></div>
+						<div class="az-header-link-block3 pull-left"><a href="#" class="az-header-link2">Онлайн заявка</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </header>
