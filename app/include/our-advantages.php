578ba71494aeca53c59627b7d5499b0586475f20
hsrazna <anzar.terek@gmail.com>
2017-05-02 17:03:31 +0300

    advantages

--- app/include/our-advantages.php
+++ app/include/our-advantages.php
@@ -1,1 +1,31 @@
-news.php
+<div class="our-advantages">
+	<h2 class="main"><span>наши Преимущества</span></h2>
+	<div class="az-adv">
+		<div class="az-adv-item">
+			<div class="az-adv-img">
+				<img src="img/az-adv.png">
+			</div>
+			<div class="az-adv-cont">
+				<h3>Для нас нет границ</h3>
+				<p>Иностранные языки + знание зарубежного законодательства = представление интересов иностранных клиентов в России, защита интересов российских клиентов на международной арене.</p>
+			</div>
+		</div>
+		<div class="az-adv-item">
+			<div class="az-adv-img">
+				<img src="img/az-adv.png">
+			</div>
+			<div class="az-adv-cont">
+				<h3>Экономия вашего времени и денег</h3>
+				<p>Решение конфликтов путем медиации</p>
+			</div>
+		</div>
+		<div class="az-adv-item">
+			<div class="az-adv-img">
+				<img src="img/az-adv.png">
+			</div>
+			<div class="az-adv-cont">
+				<h3>Индивидуальный подход</h3>
+			</div>
+		</div>
+	</div>
+</div>
