41ea0750ce8f51eab2845ab1cb556a813c5aa77f
iwebnal <iwebnal@gmail.com>
2017-05-02 17:38:00 +0300

    footer and map

--- app/include/footer.php
+++ app/include/footer.php
@@ -1,1 +1,43 @@
-footer.php
+<section class="ah-footer">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-footer-wrapp">
+					<div class="ah-footer-box1">
+						<div class="ah-footer-imgbox">
+							<a href="/"><img src="img/ah-fotlogo.png" class="img-responsive" alt="logo footer"></a>
+						</div>
+					</div>
+					<div class="ah-footer-box2">
+						<span class="ah-footbox2-span">мы в социальных сетях:</span>
+						<ul class="ah-footbox2-list">
+							<li><a href="#" class="ah-ld"></a></li>
+							<li><a href="#" class="ah-tw"></a></li>
+							<li><a href="#" class="ah-in"></a></li>
+							<li><a href="#" class="ah-fb"></a></li>
+							<li><a href="#" class="ah-vk"></a></li>
+						</ul>
+					</div>
+					<div class="ah-footer-box3">
+						<a href="#" class="ah-footer-logoimgbox"></a>
+					</div>
+					<div class="ah-footer-box4">
+						<a href="tel:88129991990" class="ah-footbox4-span">8 (812) <span>999-19-90</span></a>
+						<span class="ah-footbox4-span1">Пн-Пт с 09:00 до 19:00 Мск</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="ah-footer-bottom">
+	<div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-footer-wrappbot">
+					<span class="ah-footwrappbot-span">Copyright © 2016 «InterJus Ltd» Все права защищены.</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
