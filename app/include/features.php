92a65cf745cf2841a3232e25d769202f88d6d97a
hsrazna <anzar.terek@gmail.com>
2017-05-02 13:57:25 +0300

    feature block

--- app/include/features.php
+++ app/include/features.php
@@ -1,1 +1,62 @@
-news.php
+<div class="az-feature row">
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+		<div class="az-feature-item item1">
+			<div class="az-feature-item-inner">
+				<div class="az-feature-icon">
+					<img src="img/az-feature1.png" alt="">
+				</div>
+				<div class="az-feature-title">
+					<h3>сделки с недвижимостью</h3>
+				</div>
+				<div class="az-feature-more">
+					<a href="#">каталог</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+		<div class="az-feature-item item2">
+			<div class="az-feature-item-inner">
+				<div class="az-feature-icon">
+					<img src="img/az-feature2.png" alt="">
+				</div>
+				<div class="az-feature-title">
+					<h3>программы по иммиграции</h3>
+				</div>
+				<div class="az-feature-more">
+					<a href="#">подробнее</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+		<div class="az-feature-item item3">
+			<div class="az-feature-item-inner">
+				<div class="az-feature-icon">
+					<img src="img/az-feature3.png" alt="">
+				</div>
+				<div class="az-feature-title">
+					<h3>о правовом аудите Due diligance</h3>
+				</div>
+				<div class="az-feature-more">
+					<a href="#">подробнее</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+		<div class="az-feature-item item4">
+			<div class="az-feature-item-inner">
+				<div class="az-feature-icon">
+					<img src="img/az-feature4.png" alt="">
+				</div>
+				<div class="az-feature-title">
+					<h3>медиация</h3>
+				</div>
+				<div class="az-feature-more">
+					<a href="#">подробнее</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
