117f97b5f3a7813ebdae22f196c5dc44363e1fb3
hsrazna <anzar.terek@gmail.com>
2017-05-02 15:03:23 +0300

    form

--- app/include/ostavit-zayavku.php
+++ app/include/ostavit-zayavku.php
@@ -1,1 +1,32 @@
-news.php
+<div class="az-form">
+	<fieldset class="az-form-wrap">
+	  <legend align="center">
+	  	оставить заявку
+	  </legend>
+	  <form action="/">
+	  	<div class="az-form-col">
+			  <div class="az-form-item-wrap name">
+				  <label for="az-name">Ваше имя:</label>
+				  <input type="text" id="az-name">
+			  </div>
+		  </div>
+	  	<div class="az-form-col">
+			  <div class="az-form-item-wrap email error">
+				  <label for="az-email">Ваш e-mail:</label>
+				  <input type="text" id="az-email" value="Неправильный e-mail">
+			  </div>
+		  </div>
+	  	<div class="az-form-col">
+			  <div class="az-form-item-wrap phone">
+				  <label for="az-phone">Ваш телефон:</label>
+				  <input type="text" id="az-phone">
+			  </div>
+		  </div>
+	  	<div class="az-form-col">
+			  <div class="az-form-item-wrap2">
+				  <input type="submit" value="отправить заявку" class="az-submit">
+			  </div>
+		  </div>
+	  </form>
+	</fieldset>
+</div>
