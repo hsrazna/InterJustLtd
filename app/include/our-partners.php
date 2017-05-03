fd4600edd4ba74b0bc8b7178e2edb22cffb30425
iwebnal <iwebnal@gmail.com>
2017-05-03 08:15:57 +0300

    a

--- app/include/our-partners.php
+++ app/include/our-partners.php
@@ -1,1 +1,50 @@
-news.php
+<div class="row">
+	<div class="col-lg-12 col-md-12 col-sm-12 hidden-xs">
+		<div class="ah-slid-partners-wrapp">
+			<div class="ah-slid-partners">
+				<div class="item">
+					<div class="ah-imgbox">
+						<img src="img/partner1.png" alt="babolat">
+					</div>
+				</div>
+				<div class="item">
+					<div class="ah-imgbox">
+						<img src="img/partner2.png" alt="nokia">
+					</div>
+				</div>
+				<div class="item">
+					<div class="ah-imgbox">
+						<img src="img/partner3.png" alt="roca">
+					</div>
+				</div>
+				<div class="item">
+					<div class="ah-imgbox">
+						<img src="img/partner4.png" alt="kerama">
+					</div>
+				</div>
+				<div class="item">
+					<div class="ah-imgbox">
+						<img src="img/partner5.png" alt="google">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="ah-slid-partners-wrapp320 hidden-lg hidden-md hidden-sm">
+	<div class="ah-imgbox320">
+		<img src="img/partner1.png" alt="babolat">
+	</div>
+	<div class="ah-imgbox320">
+		<img src="img/partner2.png" alt="nokia">
+	</div>
+	<div class="ah-imgbox320">
+		<img src="img/partner3.png" alt="roca">
+	</div>
+	<div class="ah-imgbox320">
+		<img src="img/partner4.png" alt="kerama">
+	</div>
+	<div class="ah-imgbox320">
+		<img src="img/partner5.png" alt="google">
+	</div>
+</div>
--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,30 @@
 $(function() {
 
+
+	if($('.ah-slid-partners').length>0){
+
+		$(".ah-slid-partners").owlCarousel({
+		    loop:true,
+		    margin:30,
+		    nav:true,
+		    autoplay:false,
+		    smartSpeed:1000,
+		    autoplayTimeout:2000,
+		    navText:['<span class="prev_arrs1"></span>', '<span class="next_arrs1"></span>'],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:5
+		        }
+		    }
+		});
+
+	}
+
+
 });
